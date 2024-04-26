afef01fd76aca7cbdb61cae49ba2a7302358d54d
Dean Ayalon <deanayalon@me.com>
2024-04-26 20:04:46 +0300

    Modified env.sh to check .env version is up to date with defaults.env

--- env.sh
+++ env.sh
@@ -1,15 +1,30 @@
 #!/bin/bash
-# Check if .env file exists, and create it if not
+# Checks if .env file exists and is up to date; Generates it otherwise
+
+# Current version, based on defaults.env
+version=$(grep -oP '^ENV_VER=\K.*' defaults.env)
 
 # Execution context - fms-docker repo
 cd "$(dirname "$0")"
 
 # Exit if .env exists
-[ -f .env ] && exit 2
+if [ -f .env ]; then
+    source .env
+    if [ -z $ENV_VER ] || [ $ENV_VER != $version ]; then 
+        echo "Old .env file found ( $ENV_VER / $version )"
+        echo "Renaming .env -> old.env"
+        mv .env old.env
+        old=true
+    else 
+        # .env up to date
+        exit 2
+    fi
+fi
 
 # Generate .env and wait for user input
-echo .env file not found, generating...
+echo generating .env file...
 # Generate default .env
-cp "defaults.env" ".env"
-code ".env"
+cp defaults.env .env
+code .env
+[ $old ] && code old.env
 read -p "Edit the .env file, then press enter to continue, or ^C to cancel"
