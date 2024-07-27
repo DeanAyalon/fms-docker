08707552e3403c7ef2397190e1379b5cfd029db7
Dean Ayalon <dev@deanayalon.com>
2024-07-27 13:56:28 +0300

    Separate prep into compose.prep and add modify service to reimage FMS from existing version Image script now takes prep down, and won't ask to remove Sample if it already does not exist
    Prep container will no longer stay for reimaging
    Modify service will create an fms-prep container based on an already-made FMS image

--- prep/image.sh
+++ prep/image.sh
@@ -18,18 +18,22 @@ tag=$VERSION-u$UBUNTU-$PROCESSOR
 [ ! -z $TAG_PREFIX ] && tag=$TAG_PREFIX-$tag
 [ ! -z $TAG_SUFFIX ] && tag+=-$TAG_SUFFIX
 
-# Remove
-echo Remove sample FileMaker database? [Y/n]
-read no_sample
-if [ "$no_sample" != "n" ] && [ "$no_sample" != "N" ]; then
-    docker exec -w "/opt/FileMaker/FileMaker Server/Data/Databases" $container rm -rf Sample
+# Clean up
+db_path="/opt/FileMaker/FileMaker Server/Data/Databases"
+sample_db_exists=$(docker exec $container ls "$db_path" | grep Sample)
+if [ ! -z $sample_db_exists ]; then
+    echo Remove sample FileMaker database? [Y/n]
+    read remove_sample
+    if [ "$remove_sample" != "n" ] && [ "$remove_sample" != "N" ]; then
+        docker exec -w "$db_path" $container rm -rf Sample
+    fi
 fi
 
 # Commit running container into image
 echo Committing $container into $IMAGE:$tag
 docker commit $container $IMAGE:$tag
-echo Stopping $container 
-docker stop $container
+echo Removing $container 
+docker rm -f $container
 
 # Push
 if [ ! -z $REPO ]; then
